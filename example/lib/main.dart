e8d3e56d5640c0f5e05f21a1e5a823c59aa20850
Ahmed Ghanem <ahmedghanem@outlook.com>
2021-03-04 21:11:19 +0200

    update pubspec.yaml for package and example to supported null safety version of deps

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
